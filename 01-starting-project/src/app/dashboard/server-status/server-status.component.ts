1b0d75e56cd3a59e506e6b437c66929007c32d3d
Ravi <ravi89nyati@gmail.com>
2026-02-20 05:11:24 -0500

    Day 02 work

--- 01-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ 01-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private destroyRef = inject(DestroyRef);
+  //private interval?: ReturnType<typeof setInterval>;
+  ngOnInit() {
+    const interval = setInterval(() => {
+      const randomValue = Math.random();
+      if (randomValue < 0.7) {
+        this.currentStatus = 'online';
+      } else if (randomValue < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 2000);
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
+  }
+
+  // ngOnDestroy() {
+  //   clearTimeout(this.interval);
+  // }
 }
--- 01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
-
+import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  onSubmit(title: string, description: string) {
+    console.log('Form submitted');
+    console.log('Title:', title);
+    console.log('Description:', description);
+    this.form?.nativeElement.reset();
+  }
+}
